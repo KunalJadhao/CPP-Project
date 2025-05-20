cc1fc57358c99a63e048cd5f81ba04d1ad359d98
KUNAL J. <connectkunaljadhav@gmail.com>
2025-05-20 16:45:16 +0530

    Add files via upload

---
+++ Cpp Project/MyStartup.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Work {
+public:
+    int ID;
+    string title;
+    string description;
+    string postedby;
+
+    Work(int id, string t, string d, string p) :
+        ID(id), title(t), description(d), postedby(p) {}
+    
+    void displayDetails() {
+        cout << "Work ID: " << ID << endl;
+        cout << "Title: " << title << endl;
+        cout << "Description: " << description << endl;
+        cout << "Posted By: " << postedby << endl;
+    }
+};
+
+class Applicant {
+public:
+    string name;
+    string password;
+
+    Applicant(string n, string p) : name(n), password(p) {}
+};
+
+class Purple {
+public:
+    void Details() {
+        int id;
+        string title, description, postedby;
+
+        cout << "Enter Work ID: ";
+        cin >> id;
+        cin.ignore();  // To ignore leftover newline character from previous input
+
+        cout << "Enter Title: ";
+        getline(cin, title);
+
+        cout << "Enter Description: ";
+        getline(cin, description);
+
+        cout << "Enter Posted By: ";
+        getline(cin, postedby);
+
+        Work P(id, title, description, postedby);
+        P.displayDetails();
+    }
+};
+
+int main() {
+    Purple L;
+    L.Details();
+
+    return 0;
+}
